9a93019237431e8adcf04a81199737d2bd85720e
M Marinova <marusia.petrova.m@gmail.com>
2022-06-29 16:07:00 +0100

    making everything responsive, creating different states for the navigation

--- assets/js/side-nav.js
+++ assets/js/side-nav.js
@@ -1,3 +1,4 @@
+// function to open and close the side nav
 const side_nav_open_close = () => {
   const side_nav = document.querySelector('#side-nav');
   const side_nav_btn = document.querySelector('#side-nav-burger');
@@ -17,5 +18,58 @@ const side_nav_open_close = () => {
 
   });
 }
-
 side_nav_open_close();
+
+
+// finction to slide the nav on mobile
+const side_nav_slide_in = () => {
+  const side_nav = document.querySelector('#side-nav');
+  const top_nav_btn = document.querySelector('#top-nav-burger');
+  const side_nav_btn = document.querySelector('#side-nav-burger-mobile');
+
+
+  top_nav_btn.addEventListener('click',() => {
+    side_nav.style.transform = "translateX(0%)";
+  });
+
+  side_nav_btn.addEventListener('click',() => {
+    side_nav.style.transform = "translateX(-100%)";
+  });
+}
+side_nav_slide_in();
+
+// on load check screen size and if between the selected size range close the nav
+window.addEventListener('load', (event) => {
+    let window_inner_width = window.innerWidth;
+    const side_nav_btn = document.querySelector('#side-nav-burger');
+
+    if(window_inner_width < 768 && window_inner_width > 576){
+      side_nav_btn.click();
+    }
+
+});
+
+// on resize check screen size and if between the selected size range close the nav
+window.addEventListener('resize', (event) => {
+  let window_inner_width = window.innerWidth;
+  const side_nav_btn = document.querySelector('#side-nav-burger');
+  const side_nav = document.querySelector('#side-nav');
+
+  if(window_inner_width < 768 && window_inner_width > 575){
+    if (side_nav.classList.contains('side-nav--opened')){
+      side_nav_btn.click();
+    }
+    side_nav.style.transform = "translateX(0%)";
+  }
+  else if(window_inner_width < 576){
+    side_nav.classList.add('side-nav--opened');
+    side_nav.classList.remove('side-nav--closed');
+    side_nav.style.transform = "translateX(-100%)";
+  }
+  else if(window_inner_width > 767){
+    side_nav.classList.add('side-nav--opened');
+    side_nav.classList.remove('side-nav--closed');
+    side_nav.style.transform = "translateX(0%)";
+  }
+
+});
