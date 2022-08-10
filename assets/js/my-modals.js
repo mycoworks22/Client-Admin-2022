3968b0eaf83340b45ad6b980de4d0eb4119e0ada
M Marinova <marusia.petrova.m@gmail.com>
2022-08-10 09:20:04 +0100

    new company

---
+++ assets/js/my-modals.js
@@ -0,0 +1,30 @@
+function openMyModal(param) {
+  // get the modal and the modal container
+  const modalContainer = document.getElementById(param);
+  const modalInner = modalContainer.querySelector('.my-modal');
+
+  // change the display property of the modal container
+  modalContainer.classList.remove('d-none');
+  modalContainer.classList.add('d-flex');
+
+  // show the modal
+  setTimeout( function () {
+    modalInner.style.transform = 'translateY(0%)';
+  }, 100);
+}
+
+function closeMyModal(param) {
+  // get the modal and the modal container
+  const modalContainer = document.getElementById(param);
+  const modalInner = modalContainer.querySelector('.my-modal');
+
+
+  // hide modal
+  modalInner.style.transform = 'translateY(-150%)';
+
+  setTimeout( function () {
+    // hide modal container
+    modalContainer.classList.remove('d-flex');
+    modalContainer.classList.add('d-none');
+  }, 1000);
+}
---
+++ assets/js/new-company-location.js
@@ -0,0 +1,17 @@
+function newCompanyLocation () {
+
+  // get all radio btns
+  const radioLnd = document.getElementById('new-company-location-lnd');
+  const radioEd = document.getElementById('new-company-location-ed');
+  const radioIpsw = document.getElementById('new-company-location-ipsw');
+
+  if (radioLnd.checked){
+    window.location.replace("/new-company-lnd.html");
+  }
+  else if (radioEd.checked){
+    window.location.replace("/new-company-ed.html");
+  }
+  else{
+    window.location.replace("/new-company-ipsw.html");
+  }
+}
